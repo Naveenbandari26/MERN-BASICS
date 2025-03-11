c0d030ec11308b97226dd482821c5a54ce280057
Naveenbandari26 <bandarin29@gmail.com>
2025-03-11 20:09:43 +0530

    java script

---
+++ Assignments/JS/lesson-1/1.js
@@ -0,0 +1,30 @@
+//1a
+alert("Good Morning")
+
+//1b
+alert("Naveen");
+
+//1c
+10 + 5
+
+//1d
+20 - 5
+
+//1e
+2 - 2 - 5
+
+//1f
+document.body.innerHTML="Good Morning";
+
+//1g
+document.body.innerHTML="Naveen";
+
+//1h
+10 + 8 + 20
+
+//1i
+100 - 20 - 50 + 200
+
+//1j
+document.body.innerHTML="";
+
---
+++ Assignments/JS/lesson-3/3.js
@@ -0,0 +1,47 @@
+//3a
+"My name is :"
+
+//3b
+"Naveen"
+
+//3c
+"My name is :"+"Naveen"
+
+//3d
+"Total cost: $"+(5+3)
+
+//3e
+`Total cost: $${5+3}`
+
+
+//3f
+alert(`Total cost: $${5+3}`)
+
+//3g
+'Total cost: $' + (599 + 295) / 100
+
+
+//3h
+`Total cost: $ ${(599 + 295) / 100}`
+
+//3i
+alert(`Total cost: $ ${(599 + 295) / 100}`)
+
+//3j
+alert(`Total cost: $${(599 + 295) / 100}
+Thank you, come again!`);
+
+
+//3k
+`Items (${2 + 2}): $${(2 * 2095 + 2 * 799) / 100}`
+
+//3l
+`Shipping & handling: $${(499 + 499) / 100}`
+
+
+//3m
+`Total before tax: $${(2 * 2095 + 2 * 799 + 499 + 499) / 100}`
+
+
+//3n
+`Estimated tax (10%): $${Math.round((2 * 2095 + 2 * 799 + 499 + 499) * 0.1) / 100}`
