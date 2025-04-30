acca4d84ddccc9262345f7ccf5a2a1a34e0094a0
naveen-b26 <naveenbandari370@gmail.com>
2025-05-01 00:19:24 +0530

    SetUpCounter()

---
+++ Assignments/26-4-25/powerOfClosures/index.js
@@ -0,0 +1,22 @@
+function setupCounter(initialValue) {
+    let count = initialValue;
+    function increment() {
+    count++;
+    return count;
+    }
+    function decrement() {
+    count--;
+    return count;
+    }
+    return {
+    increment: increment,
+    decrement: decrement
+    };
+    }
+    const counterOne = setupCounter(5);
+    console.log(counterOne.increment());
+    console.log(counterOne.increment());
+    const counterTwo = setupCounter(10);
+    console.log(counterTwo.decrement());
+    
+    console.log(counterOne.decrement());
---
+++ Assignments/26-4-25/powerOfClosures/thought.md
@@ -0,0 +1,11 @@
+Thought: 
+
+    The thing happeing here is that the function `setupCounter()` is taking the `initialValue` then do the functionalities like increment and decrement over it.
+
+    Then it returns the both the funtionalities
+
+    Then by assinging to vaianble, will able to access the counter value.
+
+    the count variable will be different for the both `counterOne` and `counterTwo` 
+
+    as the closure has the property of state maintainence so the data will be stored respective to the variable.
